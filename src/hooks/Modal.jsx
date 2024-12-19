129b9f4e5c51392dc3dacb24608880a64f8c16bb
jerssonarleyosma <jerssonarleyosma@gmail.com>
2024-12-18 21:42:15 -0500

    add(directories): make directories for structuring atomic desing
    also add modal component and then create a issue for made scalable it

---
+++ src/hooks/Modal.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Modal = () => {
+  return (
+    <div>
+        <div className="modal">
+            <div className="modal-content">
+                <span className="close-button">&times;</span>
+                <h2>Modal Title</h2>
+                <p>This is a modal window.</p>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Modal
